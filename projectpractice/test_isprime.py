e2522d51d31ab030fc69f5504570c109be6ee77c
raneemousa3 <123393061+raneemousa3@users.noreply.github.com>
2024-02-27 18:54:20 -0500

    Update test_isprime.py

--- projectpractice/test_isprime.py
+++ projectpractice/test_isprime.py
@@ -2,4 +2,8 @@ from .Practice_ import is_prime
 def test_isprime_():
     assert is_prime(6) == False
     assert is_prime(23) == True
+    assert is_prime(98) == False
+    assert is_prime(8) == False
+    assert is_prime(1) == False
+    assert is_prime(91) == False
 
